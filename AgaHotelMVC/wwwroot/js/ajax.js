6d723c8a2447a204e432c9fe6d70d838c574e904
Agasef Memmedli <mmaries221@gmail.com>
2019-09-16 20:50:38 +0400

    Allaha and olsun Last Final Commit

--- AgaHotelMVC/wwwroot/js/ajax.js
+++ AgaHotelMVC/wwwroot/js/ajax.js
@@ -3,26 +3,7 @@
     invoiceFullPrice();
 
 
-    //function setInputFilter(textbox, inputFilter) {
-    //    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
-    //        textbox.addEventListener(event, function () {
-    //            if (inputFilter(this.value)) {
-    //                this.oldValue = this.value;
-    //                this.oldSelectionStart = this.selectionStart;
-    //                this.oldSelectionEnd = this.selectionEnd;
-    //            } else if (this.hasOwnProperty("oldValue")) {
-    //                this.value = this.oldValue;
-    //                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
-    //            }
-    //        });
-    //    });
-    //}
 
-
-    //// Install input filters.
-    //setInputFilter(document.getElementById("uintTextBox"), function (value) {
-    //    return /^\d*$/.test(value);
-    //});
     var product = $(".hidden-product");
     product.hide();
     var searchList = $(".search-list");
